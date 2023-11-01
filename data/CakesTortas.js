30ecfb62dfc8f465be659a65549d19c17e1f49b7
rfe10000 <rf10000@hotmail.com>
2023-11-01 12:22:58 +0100

    first commit

---
+++ data/CakesTortas.js
@@ -0,0 +1,18 @@
+let cakesTortas = '{"cakes":[' +
+'{"ID":"21","Type":"Cupcake","Name":"Cupcake 21","Price":"7.5","Img":"cupcake1 (1).jpg" },' +
+'{"ID":"22","Type":"Cupcake","Name":"Cupcake 22","Price":"8","Img":"cupcake2 (1).jpg" },' +
+'{"ID":"23","Type":"Cupcake","Name":"Cupcake 23","Price":"14","Img":"cupcake3 (1).jpg" },' +
+'{"ID":"24","Type":"Cupcake","Name":"Cupcake 24","Price":"11","Img":"cupcake4 (2).jpg" },' +
+'{"ID":"25","Type":"Cupcake","Name":"Cupcake 25","Price":"8.3","Img":"cupcake5 (2).jpg" },' +
+'{"ID":"26","Type":"Cupcake","Name":"Cupcake 26","Price":"10","Img":"cupcake6 (2).jpg" },' +
+'{"ID":"27","Type":"Cupcake","Name":"Cupcake 27","Price":"11.6","Img":"cupcake7 (2).jpg" },' +
+'{"ID":"28","Type":"Cupcake","Name":"Cupcake 28","Price":"12","Img":"cupcake8 (2).jpg" },' +
+
+'{"ID":"31","Type":"Torta","Name":"Torta 31","Price":"23","Img":"torta1 (2).png" },' +
+'{"ID":"32","Type":"Torta","Name":"Torta 32","Price":"21.5","Img":"torta2 (2).png" },' +
+'{"ID":"33","Type":"Torta","Name":"Torta 33","Price":"19","Img":"torta3 (2).jpg" },' +
+'{"ID":"34","Type":"Torta","Name":"Torta 34","Price":"18.4","Img":"torta4 (1).png" },' +
+'{"ID":"35","Type":"Torta","Name":"Torta 35","Price":"20","Img":"torta5 (2).jpg" },' +
+'{"ID":"36","Type":"Torta","Name":"Torta 36","Price":"22","Img":"torta6 (2).png" },' +
+'{"ID":"37","Type":"Torta","Name":"Torta 37","Price":"27","Img":"torta7 (2).png" },' +
+'{"ID":"38","Type":"Torta","Name":"Torta 38","Price":"19.8","Img":"torta8 (2).jpg" }]}';
